e09c17f89062f72191948493b5d95a85336c85d0
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-09-28 10:05:05 +0530

    apply add employee feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeListComponent } from './component/employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './component/create-employee/create-employee.component';
 
 const routes: Routes = [
   {
     path: 'employees',
     component: EmployeeListComponent,
   },
+  { path: 'create-employee', component: CreateEmployeeComponent },
   {
     path: '',
     redirectTo: 'employees',
--- src/app/model/employee.ts
+++ src/app/model/employee.ts
@@ -1,5 +1,5 @@
 export class Employee {
-    id: number;
+    // id: number;
     name: string;
     address: string;
     contact: string;
